72da501bf336a345be55966aa1942a075855d01b
Uncool <106897936+Uncool-dudes@users.noreply.github.com>
2024-10-22 14:31:25 +0530

    Scaffolded rust project

---
+++ back/target/debug/deps/back-0d1c1e20a1b2e756.d
@@ -0,0 +1,5 @@
+/Users/uncool/ERP/back/target/debug/deps/libback-0d1c1e20a1b2e756.rmeta: src/main.rs
+
+/Users/uncool/ERP/back/target/debug/deps/back-0d1c1e20a1b2e756.d: src/main.rs
+
+src/main.rs:
---
+++ back/target/debug/deps/back-d3ed4d36be2956d5.d
@@ -0,0 +1,5 @@
+/Users/uncool/ERP/back/target/debug/deps/libback-d3ed4d36be2956d5.rmeta: src/main.rs
+
+/Users/uncool/ERP/back/target/debug/deps/back-d3ed4d36be2956d5.d: src/main.rs
+
+src/main.rs:
